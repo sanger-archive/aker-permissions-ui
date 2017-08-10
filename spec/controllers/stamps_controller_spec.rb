0528df7b2e7bca9d64085c0825ff79aace0449e1
Harriet Craven <hc6@sanger.ac.uk>
2017-08-10 16:58:01 +0100

    Cleaned up stamp controller tests

--- spec/controllers/stamps_controller_spec.rb
+++ spec/controllers/stamps_controller_spec.rb
@@ -3,10 +3,6 @@ require 'rails_helper'
 RSpec.describe StampsController, type: :controller do
 
   let(:user) { create(:user, email: 'jeff') }
-  let(:url) { Rails.configuration.stamp_url }
-  let(:content_type) { 'application/vnd.api+json' }
-  let(:request_headers) { { 'Accept' => content_type, 'Content-Type'=> content_type } }
-  let(:response_headers) { { 'Content-Type' => content_type } }
 
   before :each do
     sign_in(user)
@@ -97,80 +93,27 @@ RSpec.describe StampsController, type: :controller do
 
   describe 'PUT #update' do
     before :each do
-      @oldname = "oldname"
-      stamp_data = { id: SecureRandom.uuid, type: "stamps", attributes: { name: @oldname, 'owner_id': user.email }}
-
-      stub_request(:post, url+"stamps")
-         .with(body: { data: { type: "stamps", attributes: { name: @oldname }}}.to_json, headers: request_headers)
-         .to_return(status: 200, body: { data: stamp_data }.to_json, headers: response_headers)
-
-      @stamp = build(:stamp, name: @oldname)
-      @stamp.save
+      @stamp1 = double('stamp', id: SecureRandom.uuid, name: 'stamp1', owner_id: 'jeff')
     end
 
-    context "when the stamp does not have permissions to update" do
-      it "it should update the stamp" do
-        controller.instance_variable_set(:@stamp, @stamp)
-        owner = user.email
-        newname = "newstampname"
+    it "it should update the stamp" do
+      controller.instance_variable_set(:@stamp, @stamp1)
+      allow(StampClient::Stamp).to receive(:find_with_permissions).and_return([@stamp1])
+      allow_any_instance_of(StampForm).to receive(:save).and_return(true)
 
-        old_response_body = make_stamp_without_permission_data(@stamp.id, @oldname, owner)
-
-        stub_request(:get, url+"stamps/"+@stamp.id+"?include=permissions")
-          .with(headers: request_headers )
-          .to_return(status: 200, body: old_response_body.to_json, headers: response_headers)
-
-        new_stamp_data = { id: @stamp.id, type: "stamps", attributes: { name: newname }}
-
-        stub_request(:patch, url+"stamps/"+@stamp.id)
-          .with(body: { data: { id: @stamp.id, type: "stamps", attributes: { name: newname }}}.to_json, headers: request_headers)
-          .to_return(status: 200, body: { data: new_stamp_data }.to_json, headers: response_headers)
-
-        new_response_body = make_stamp_without_permission_data(@stamp.id, newname, owner)
-
-        stub_request(:post, url+"stamps/"+@stamp.id+"/set_permissions")
-          .with(body: { data: []}.to_json, headers: request_headers)
-          .to_return(status: 200, body: new_response_body.to_json, headers: response_headers)
-
-        put :update, params: { id: @stamp.id, stamp: { name: "newstampname" } }
-        expect(flash[:success]).to match('Stamp updated')
-      end
+      get :update, params: { id: @stamp1.id, stamp: { name: "newstampname" } }
+      expect(flash[:success]).to match('Stamp updated')
     end
 
+    it "it should not update the stamp" do
+      controller.instance_variable_set(:@stamp, @stamp1)
+      allow(StampClient::Stamp).to receive(:find_with_permissions).and_return([@stamp1])
+      allow_any_instance_of(StampForm).to receive(:save).and_return(false)
 
-    context "when the stamp does have permissions to update" do
-      it "it should update the stamp" do
-        controller.instance_variable_set(:@stamp, @stamp)
-        owner = user.email
-        old_permission_id = "1"
-        old_permission_type = :consume
-        old_permitted = 'xyz'
-
-        old_response_body = make_stamp_with_permission_data(@stamp.id, @stamp.name, owner, old_permission_id, old_permitted, old_permission_type)
-
-        stub_request(:get, url+"stamps/"+@stamp.id+"?include=permissions")
-          .with(headers: request_headers )
-          .to_return(status: 200, body: old_response_body.to_json, headers: response_headers)
-
-        stub_request(:patch, url+"stamps/"+@stamp.id)
-           .with(body: { data: { id: @stamp.id, type: "stamps", attributes: {}}}.to_json, headers: request_headers)
-           .to_return(status: 200, body: "", headers: {})
-
-        new_permission_id = "2"
-        new_permission_type = :edit
-        new_permitted = 'dirk@sanger.ac.uk'
-
-        stub_data = { data: [ {permitted: new_permitted, 'permission-type': new_permission_type}] }
-        new_response_body = make_stamp_with_permission_data(@stamp.id, @stamp.name, owner, new_permission_id, new_permitted, new_permission_type)
-
-        stub_request(:post, url+"stamps/"+@stamp.id+"/set_permissions")
-            .with(body: stub_data.to_json, headers: request_headers)
-            .to_return(status: 200, body: new_response_body.to_json, headers: response_headers)
-
-        put :update, params: { id: @stamp.id, stamp: { name: @oldname, user_editors: 'dirk'} }
-        expect(flash[:success]).to match('Stamp updated')
-      end
+      get :update, params: { id: @stamp1.id, stamp: { name: "newstampname" } }
+      expect(flash[:danger]).to match('Failed to update stamp')
     end
+
   end
 
   describe 'DELETE #destroy' do
@@ -188,67 +131,6 @@ RSpec.describe StampsController, type: :controller do
       expect(@stamp1.destroy).not_to be_nil
       expect(flash[:success]).to match('Stamp deleted')
     end
-
-  end
-
-
-  def make_stamp_with_permission_data(stamp_id, stamp_name, owner, permission_id, permitted, permission_type)
-    {
-      data:
-      {
-        id: stamp_id,
-        type: "stamps",
-        attributes:
-        {
-          name: stamp_name,
-          "owner-id": owner
-        },
-        relationships:
-        {
-          permissions:
-          {
-            data: [{ type: "permissions", id: permission_id}]
-          }
-        }
-      },
-      included:
-      [
-        {
-          id: permission_id,
-          type: "permissions",
-          attributes:
-          {
-            "permission-type": permission_type,
-            permitted: permitted,
-            "accessible-id": stamp_id
-          }
-        }
-      ]
-    }
-  end
-
-  def make_stamp_without_permission_data(stamp_id, stamp_name, owner)
-    {
-      data:
-      {
-        id: stamp_id,
-        type: "stamps",
-        attributes:
-        {
-          name: stamp_name,
-          "owner-id": owner
-        },
-        relationships:
-        {
-          permissions:
-          {
-            data: []
-          }
-        }
-      },
-      included:
-      []
-    }
   end
 
 end
