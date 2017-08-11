cb426439ac4f106a75428e0ca51c602270fa53d1
Eduardo Martin Rojo <emr@sanger.ac.uk>
2017-08-11 16:25:24 +0100

    ldap

--- config/application.rb
+++ config/application.rb
@@ -15,11 +15,13 @@ module AkerStampsUi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.ldap = config_for(:ldap)
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+
   end
 
-  config.ldap = config_for(:ldap)
 end
