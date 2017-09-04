c433526a08dd87d05dc122bfcbd1222ad76288c8
Eduardo Martin Rojo <emr@sanger.ac.uk>
2017-09-04 11:42:13 +0100

    Removes config_ldap line

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,6 @@ module AkerStampsUi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    config.ldap = config_for(:ldap)
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
