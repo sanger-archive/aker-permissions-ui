e5d6d988b98cc28a3541add6efb8defff1fda6a5
Alex Cummins <alex.cummins@sanger.ac.uk>
2017-10-23 16:12:40 +0100

    Add dropdown menu

--- config/application.rb
+++ config/application.rb
@@ -15,12 +15,23 @@ module AkerStampsUi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    if Rails.env.production? || Rails.env.staging?
+      config.urls = { submission: "https://dev.psd.sanger.ac.uk:9002/submission",
+                      permissions: "https://dev.psd.sanger.ac.uk:9009/stamps-ui",
+                      sets: "https://dev.psd.sanger.ac.uk:9001/set-shaper",
+                      projects: "https://dev.psd.sanger.ac.uk:9003/study",
+                      work_orders: "https://dev.psd.sanger.ac.uk:9004/work-orders" }
+    elsif Rails.env.development? || Rails.env.test?
+      config.urls = { submission: "",
+                      permissions: "",
+                      sets: "",
+                      projects: "",
+                      work_orders: "" }
+    end
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
-
   end
-
 end
